c3ab16f028a9f0a1e6b82bdecefbbf9480432e53
Simone <simone.bervi@gmail.com>
2021-06-06 17:55:08 +0200

    Fixed button calculator

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: MyHomePage(title: 'Flutter Calculator'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Widget createRow(
+    String title1,
+    String title2,
+    String title3,
+    String title4,
+  ) {
+    return Expanded(
+      child: Row(
+        children: [
+          createButton(title1),
+          createButton(title2),
+          createButton(title3),
+          createButton(title4),
+        ],
+      ),
+    );
+  }
+
+  Widget createButton(String title) {
+    return Expanded(
+      child: ButtonTheme(
+        height: double.infinity,
+        child: OutlineButton(
+          onPressed: null,
+          child: Text(title),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Align(
+                    alignment: FractionalOffset.bottomRight,
+                    child: Text('0'),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  createRow('+', ' -', '*', ' /'),
+                  createRow('7', '8', '9', '0'),
+                  createRow('4', '5', '6', 'CE'),
+                  createRow('1', '2', '3', '='),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
